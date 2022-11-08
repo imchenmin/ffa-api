d9bd2ea2981bce1882f704accfa77fc2e4ca839f
chenminken <zbcmb@outlook.com>
2022-11-08 22:52:17 +0800

    add ws

--- fast_firs_aid_server/database.py
+++ fast_firs_aid_server/database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://eedtxbzpafwwpv:2b11ddd917be1cbaa9ad3063a4cfa564bc1c4f5739cc9f77ab260e382c81985a@ec2-52-71-69-66.compute-1.amazonaws.com:5432/da84mjkam2mdkf"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://eedtxbzpafwwpv:2b11ddd917be1cbaa9ad3063a4cfa564bc1c4f5739cc9f77ab260e382c81985a@ec2-52-71-69-66.compute-1.amazonaws.com:5432/da84mjkam2mdkf"
+SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Request, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from datetime import datetime, timedelta
@@ -8,12 +9,31 @@ from typing import Union
 from fast_firs_aid_server import models, crud, schemas, mypassword
 from fast_firs_aid_server.database import SessionLocal, engine
 
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+@app.middleware("http")
+async def TestCustomMiddleware(request: Request, call_next):
+    the_headers = request.headers
+    print(the_headers)
+
+    response = await call_next(request)
+    return response
+# async def log_request_info(request: Request):
+#     print(request.headers)
+#     request_body = await request.json()
+#     logger.info(
+#         f"{request.method} request to {request.url} metadata\n"
+#         f"\tHeaders: {request.headers}\n"
+#         f"\tBody: {request_body}\n"
+#         f"\tPath Params: {request.path_params}\n"
+#         f"\tQuery Params: {request.query_params}\n"
+#         f"\tCookies: {request.cookies}\n"
+#     )
 
 
 # Dependency
@@ -23,9 +43,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-
 
 
 def authenticate_user_by_id(db, user_id: str, password: str):
@@ -58,7 +75,7 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     return encoded_jwt
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme),  db: Session = Depends(get_db)):
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -93,7 +110,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @app.post("/token", response_model=schemas.Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),  db: Session = Depends(get_db)):
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user_by_phone_number(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
@@ -134,7 +151,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 @app.post("/users/{user_id}/items/", response_model=schemas.Item)
 def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+        user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
@@ -145,14 +162,99 @@ def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     print(items)
     return items
 
+
 # TODO 添加只有本人能看到
 @app.get("/users/{user_id}/aid_items/", response_model=list[schemas.AidItem])
 def read_items(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_aid_items_by_initiator_id(db, initiator_id=user_id, skip=skip, limit=limit)
     return items
 
+
 @app.post("/users/{user_id}/aid_items/", response_model=schemas.AidItem)
 def create_aid_item_for_user(
-    user_id: int, aid_item: schemas.AidItemCreate, db: Session = Depends(get_db)
+        user_id: int, aid_item: schemas.AidItemCreate, db: Session = Depends(get_db)
 ):
     return crud.create_user_aid_item(db=db, aid_item=aid_item, user_id=user_id)
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://10.27.132.158:8012/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/wstest/")
+async def get():
+    return HTMLResponse(html)
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    # current_user = crud.get_user(db=get_db(),user_id=user_id)
+    # print(current_user)
+    # if current_user is None:
+    #     return
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
+
+# app.include_router(api_router, dependencies=[Depends(log_request_info)])
